abb272a131c84d8563ef364814530d5fd7540e9f
neguscollis12 <neguscollis12@gmail.com>
2022-02-07 12:27:18 -0800

    TestNG Auto Framework

---
+++ src/test/java/com/automation/tests/BaseTest.java
@@ -0,0 +1,29 @@
+package com.automation.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public  void setUp(){
+        //Setting path to chromedriver
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver");
+        // Open the browser
+        driver = new ChromeDriver();
+        // 1. To open url in browser
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+    }
+
+
+    @AfterMethod
+    public void cleanUp(){
+        driver.quit();
+    }
+
+}
